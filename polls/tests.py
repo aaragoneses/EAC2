2ea330b64983d47fb2c9f2cfcfeb04889e0d0f12
isard <isard@debian>
2025-03-30 13:10:50 +0200

    configuració Github Actions

--- polls/tests.py
+++ polls/tests.py
@@ -11,6 +11,7 @@ class MySeleniumTests(StaticLiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
         opts = Options()
+        opts.add_argument("--headless")  # Activar modo headless
         cls.selenium = WebDriver(options=opts)
         cls.selenium.implicitly_wait(5)
         cls.live_server_url = 'http://0.0.0.0:8000'  # Sobrescribimos la URL del servidor
